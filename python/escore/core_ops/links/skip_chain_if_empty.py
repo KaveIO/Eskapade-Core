3df15ef4700b5b47069abd6bda74f0dd9b052aa7
Max Baak <maxbaak@gmail.com>
2019-01-04 15:22:09 +0100

    Update: several small updates to existing links in core_ops
    - Link skip_chain_if_empty no longer assumes pandas dataframe
    - import_data_store: imports new datastore in meant-to way
      Another new feature, update, has been added to import_data_store link
      does not overwrite existing datastore, but updates it with the new
      (external) datastore contents.
    - BreakLink: added option to send BreakChain instead of Failure signal
    - Cleanup of link skip_chain_if_empty
      Link Now has new, simpler attributes:
      * skip_missing, default is true
      * skip_zero_len, default is true

--- python/escore/core_ops/links/skip_chain_if_empty.py
+++ python/escore/core_ops/links/skip_chain_if_empty.py
@@ -31,17 +31,16 @@ class SkipChainIfEmpty(Link):
         :param str name: name of link
         :param list collection_set: datastore keys holding the datasets to be checked. If any of these is empty,
                                     the chain is skipped.
-        :param bool skip_chain_when_key_not_in_ds: skip the chain as well if the dataframe is not present in the
-                                                   datastore. When True and if type is 'pandas.DataFrame', sends
-                                                   a SkipChain signal if key not in DataStore
+        :param bool skip_missing: skip the chain if the dataframe is not present in the
+                                  datastore. Default is True.
+        :param bool skip_zero_len: skip the chain if the object is found in the datastore but has zero length. Default is True.
         :param bool check_at_initialize: perform dataset empty is check at initialize. Default is true.
-        :param bool check_at_execute: perform dataset empty is check at initialize. Default is false.
         """
         Link.__init__(self, kwargs.pop('name', 'SkipChainIfEmpty'))
 
         # process keyword arguments
-        self._process_kwargs(kwargs, collection_set=[], skip_chain_when_key_not_in_ds=False, check_at_initialize=True,
-                             check_at_execute=False)
+        self._process_kwargs(kwargs, collection_set=[], skip_missing=True, skip_zero_len=True,
+                             check_at_initialize=True, check_at_execute=False)
         self.check_extra_kwargs(kwargs)
 
     def initialize(self):
@@ -56,33 +55,40 @@ class SkipChainIfEmpty(Link):
 
         Skip to the next Chain if any of the input dataset is empty.
         """
-        if self.check_at_execute:
+        if not self.check_at_initialize or self.check_at_execute:
             return self.check_collection_set()
 
         return StatusCode.Success
 
     def check_collection_set(self):
-        """Check existence of collection in either mongo or datastore, and check that they are not empty.
+        """Check existence of collection in the datastore, and check if they are empty.
 
-        Collections need to be both present and not empty.
-
-        - For mongo collections a dedicated filter can be applied before doing the count.
-        - For pandas dataframes the additional option 'skip_chain_when_key_not_in_ds' exists. Meaning,
-          skip the chain as well if the dataframe is not present in the datastore.
+        All collections need to be both present and not empty for the chain to be continued.
         """
-        # check if collection names are present in datastore
         ds = process_manager.service(DataStore)
+
+        # check if requested collection names are present in datastore
+        # if anyone is missing, skip the chain
         for k in self.collection_set:
             if k not in ds:
-                if self.skip_chain_when_key_not_in_ds:
-                    self.logger.warning('Key {key!s} not in DataStore. Sending skip chain signal.', key=k)
+                if self.skip_missing: # default is true
+                    self.logger.info('Key {key!s} not in DataStore. Skipping chain as requested.', key=k)
                     return StatusCode.SkipChain
                 else:
-                    raise Exception('Key "{key}" not in DataStore.'.format(key=k))
-            df = ds[k]
-            if len(df.index) == 0:
-                self.logger.warning(
-                    'pandas.DataFrame with datastore key "{key!s}" is empty. Sending skip chain signal.', key=k)
-                return StatusCode.SkipChain
+                    self.logger.warning('Key {key!s} not in DataStore. Continuing nonetheless.', key=k)
+                    continue
+            obj = ds[k]
+            if self.skip_zero_len: # default is true
+                try:
+                    obj_length = len(obj)
+                    if obj_length == 0:
+                        self.logger.info('object with key {key} has zero length. Skipping chain as requested.', key=k)
+                        return StatusCode.SkipChain
+                    else:
+                        self.logger.debug('object with key {key} has length {n}. Continuing.', key=k, n=obj_length)
+                        continue
+                except Exception as e: #TypeError:
+                    self.logger.error('object with key {key} has no len() function.', key=k)
+                    raise e
 
         return StatusCode.Success
