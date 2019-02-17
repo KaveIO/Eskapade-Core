51bf3f860362dbc115b6e2001c540cb5f93835f1
Max Baak <maxbaak@gmail.com>
2019-02-17 16:58:33 +0100

    Update: apply_func link uses expanded new ds.get() functionality

--- python/escore/core_ops/links/apply_func.py
+++ python/escore/core_ops/links/apply_func.py
@@ -27,17 +27,28 @@ class ApplyFunc(Link):
 
         :param str name: name of link
         :param str read_key: key of input data to read from data store
-        :param str store_key: key of output data to store in data store
+        :param default: The default value of the key in case not found.
+        :param assert_type: if set, check object for given type or tuple of types. If fails, raise TypeError.
+        :param bool assert_len: if true, check that object has length greater than 0. If fails, raise TypeError or AssertionError.
+        :param bool assert_in: assert that key is known, default is true.
         :param func: function to execute
         :param args: all args are passed pass to function as args.
         :param kwargs: all other key word arguments are passed on to the function as kwargs.
+        :param str store_key: key of output data to store in data store
         """
         # initialize Link, pass name from kwargs
         Link.__init__(self, kwargs.pop('name', 'ApplyFunc'))
 
         # Process and register keyword arguments. If the arguments are not given, all arguments are popped from
         # kwargs and added as attributes of the link. Otherwise, only the provided arguments are processed.
-        self._process_kwargs(kwargs, read_key='', store_key='', func=None)
+        self._process_kwargs(kwargs,
+                             read_key = '',
+                             default = None,
+                             assert_type = None,
+                             assert_len = False,
+                             assert_in = True,
+                             func = None,
+                             store_key='')
 
         # pass on remaining kwargs to pandas reader
         self.args = copy.deepcopy(args)
@@ -50,10 +61,10 @@ class ApplyFunc(Link):
         :rtype: StatusCode
         """
         if not isinstance(self.read_key, (str, list)):
-            raise AssertionError('read_key should be a string or list of strings.')
+            raise TypeError('read_key should be a string or list of strings.')
         self.check_arg_types(store_key=str)
         if self.func is None:
-            raise AssertionError('Input function not set correctly.')
+            raise AssertionError('Input function not set.')
 
         return StatusCode.Success
 
@@ -68,13 +79,9 @@ class ApplyFunc(Link):
         if self.read_key:
             # fetch and check input
             if isinstance(self.read_key, str):
-                obj = ds.get(self.read_key, None)
-                if obj is None:
-                    raise RuntimeError('object {} not found in datastore'.format(self.read_key))
+                obj = ds.get(self.read_key, self.default, self.assert_type, self.assert_len, self.assert_in)
             elif isinstance(self.read_key, list):
-                obj = [ds.get(key, None) for key in self.read_key]
-                if any(o is None for o in obj):
-                    raise RuntimeError('any of objects {} not found in datastore'.format(self.read_key))
+                obj = [ds.get(key, self.default, self.assert_type, self.assert_len, self.assert_in) for key in self.read_key]
             # apply function
             trans_obj = self.func(obj, *self.args, **self.kwargs)
         else:
